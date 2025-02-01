3cb61c6102859deacd0757a1d803e8363830f00e
Oleh Bilynskyi <obslinskij@gmail.com>
2025-02-01 13:13:10 +0200

    updated system output

--- HomeWork/src/hw2lms/Main.java
+++ HomeWork/src/hw2lms/Main.java
@@ -2,6 +2,6 @@ package hw2lms;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
